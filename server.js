1088c82c718528cbb60d3eabf52b2fcda95c3f88
Jin Woo Kim <jinwoov@gmail.com>
2020-01-23 13:45:53 -0800

    added where I left off

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ require('ejs');
 require('dotenv').config();
 const PORT = process.env.PORT || 3001;
 const pg = require('pg');
+const methodOverride = require('method-override');
 
 
 app.set('view engine', 'ejs');
 
 // body parser
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
 /////// Client
 
@@ -25,22 +27,29 @@ app.get('/searches/new', bookSearch);
 app.post('/searches/new', bookResults);
 app.post('/books', addFavBook);
 app.get('/books/:book_id', getOneBook);
-// app.post('/', getBooks);
+app.get('/', getBooks);
+app.put('/update/:book_id', updateBook)
 
-// function search(request, response) {
-//   response.status(200).render('./pages/index');
-// }
+function updateBook(request, response) {
+  let { image, title, author, isbn, summary, bookshelf } = request.body;
+  let SQL = `UPDATE bookApp SET author=$1, title=$2, isbn=$3, image_url=$4, descriptions=$5, bookshelf=$6 WHERE id=$7;`;
+  let safeValue = [author, title, isbn, image, summary, bookshelf];
+
+  client.query(SQL, safeValue)
+    .then(response.redirect(`/books/${request.params.book_id}`))
+    .catch(err => errorHandler(err, response));
+}
 
 function bookSearch(request, response) {
   response.status(200).render('./pages/searches/new');
 }
 
-// function getBooks(request,response) {
-//   let SQL = 'SELECT * FROM bookApp;';
-//   return client.query(SQL)
-//     .then(results => response.render('./pages/index', { result: results.rows }))
-//     .catch(err => errorHandler(err, response))
-// }
+function getBooks(request,response) {
+  let SQL = 'SELECT * FROM bookApp;';
+  return client.query(SQL)
+    .then(results => response.render('./pages/index', { result: results.rows }))
+    .catch(err => errorHandler(err, response))
+}
 
 function bookResults(request, response) {
   let bookResult = request.body.search;
@@ -83,8 +92,8 @@ function getOneBook(request,response) {
   let SQL = 'SELECT * FROM bookApp where id=$1;';
   let values = [request.params.book_id];
   client.query(SQL, values)
-     .then(result => {
-      console.log(result)
+    .then(result => {
+      // console.log(result)
       response.render('./pages/books/show', { oneBook: result.rows[0]});
     }).catch(() => {
       errorHandler('Sorry, its invalid search',request ,response)
@@ -118,6 +127,8 @@ function errorHandler(err, request, response) {
 function developerErrorHandler(request,response) {
   response.status(404).send ('sorry this request is not available yet')
 }
+
+
 
 
 
